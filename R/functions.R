43ce2c97e95ad56831014a23f54b1e828923cc9a
dzvoti <libertydzvotimlambo@gmail.com>
2024-03-18 13:49:46 +0000

    feat: Add Function to Enforce Numeric Data Type on Columns
    This commit introduces a new function, `enforceNumeric`, in `functions.R`. This function takes a dataframe and a vector of column names as input, and converts the specified columns to numeric data type. This addition improves data handling by ensuring that the data in specified columns is of the correct type for numerical computations or analyses."

--- R/functions.R
+++ R/functions.R
@@ -107,3 +107,26 @@ checkData <- function() {
         return(FALSE)
     }
 }
+
+
+
+#' Enforce Numeric Data Type for Specified Columns
+#'
+#' This function converts specified columns of a dataframe to numeric data type.
+#'
+#' @param df A dataframe.
+#' @param cols A vector of column names that should be converted to numeric.
+#'
+#' @return A dataframe with the specified columns converted to numeric.
+#'
+#' @examples
+#' \dontrun{
+#' df <- data.frame(a = c("1", "2", "3"), b = c("4", "5", "6"))
+#' df <- enforceNumeric(df, c("a", "b"))
+#' }
+#'
+#' @export
+enforceNumeric <- function(df, cols) {
+    df <- df |> dplyr::mutate_at(cols, as.numeric)
+    return(df)
+}
