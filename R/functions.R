67db25f69bfc09913cfdbb4cbf8ebb53e084594c
dzvoti <libertydzvotimlambo@gmail.com>
2024-05-16 11:32:25 +0100

    refactor: Clean up `calculateBaselinePrevalenceHighIntakeRiskAfe` function by removing unnecessary comments
    - Removed redundant and unnecessary comments from the `calculateBaselinePrevalenceHighIntakeRiskAfe` function.
    - Kept essential comments for better code readability and understanding.
    - Ensured the function's logic remains unchanged while improving code clarity.

--- R/functions.R
+++ R/functions.R
@@ -624,7 +624,6 @@ calculateFoodVehicleHouseholdReach <- function(householdConsumptionDf = househol
     return(foodVehicleHouseholdReach)
 }
 
-## TODO: Update 16 05 2024
 #' Calculate Baseline Nutrient Inadequacy (AFE Method)
 #'
 #' This function calculates the baseline inadequacy of nutrients for different administrative groups using the Adequate Food Energy (AFE) Method.
@@ -655,28 +654,23 @@ calculateBaselinePrevalenceHighIntakeRiskAfe <- function(
     intakeThresholdsDf = intakeThresholds,
     aggregationGroup = c("admin0Name", "admin1Name"),
     MNList = c("Ca", "Carbohydrates", "Cu", "Energy", "Fat", "Fe", "Fibre", "I", "IP6", "Mg", "Protein", "Se", "Zn", "Ash", "B6", "B2", "D", "N", "K", "P", "Moisture", "Cholesterol", "E", "Na", "A", "C", "B12", "B1", "B3", "B9", "B5", "B7", "Mn")) {
-    # Define required columns
     requiredConsumptionCols <- c("householdId", "amountConsumedInG")
     requiredDetailsCols <- c("householdId", "memberCount")
     requiredNctCols <- c("micronutrientId")
     requiredIntakeCols <- c("nutrient", "ul")
 
-    # Check if MNList is a character vector
     if (!is.character(MNList)) {
         stop("MNList must be a character vector e.g. c('A', 'Ca')")
     }
 
-    # Check if aggregationGroup is a character vector
     if (!is.character(aggregationGroup)) {
         stop("aggregationGroup must be a character vector e.g. c('admin0Name', 'admin1Name')")
     }
 
-    # Check if MNList and aggregationGroup are not empty
     if (length(aggregationGroup) == 0) {
         stop("aggregationGroup cannot be empty")
     }
 
-    # Check if input dataframes have required columns
     if (!all(requiredConsumptionCols %in% names(householdConsumptionDf))) {
         stop(paste("householdConsumptionDf must contain the following columns:", paste(requiredConsumptionCols, collapse = ", ")))
     }
@@ -693,113 +687,70 @@ calculateBaselinePrevalenceHighIntakeRiskAfe <- function(
         stop(paste("intakeThresholdsDf must contain the following columns:", paste(requiredIntakeCols, collapse = ", ")))
     }
 
-    # Use the createMasterNct function to create a master NCT
     masterNCT <- effectivenessCalculations::createMasterNct(nctList)
 
-    ## Create a wider format for the intakeThresholds
     upperIntakeLevel <- intakeThresholdsDf |>
         dplyr::select(nutrient, ul) |>
-
         dplyr::filter(!is.na(ul)) |>
-
         dplyr::filter(nutrient %in% MNList) |>
         tidyr::pivot_wider(names_from = nutrient, values_from = ul) |>
-
         dplyr::mutate_all(as.numeric) |>
-
         dplyr::rename_with(~ paste0(., "SupplyAfeUpperIntakeLevel"), dplyr::everything())
 
-    # Process the consumption data
-    # Load the consumption data
     enrichedHouseholdConsumption <- householdConsumptionDf |>
-
         tibble::as_tibble() |>
-        # Join the household details to the consumption data (Joining columns with the same name)
         dplyr::left_join(householdDetailsDf) |>
-        # Join the master NCT to the consumption data
         dplyr::left_join(masterNCT) |>
-        # Convert all columns needed for calculations to numeric
         dplyr::mutate_at(c("amountConsumedInG", "afeFactor", MNList), as.numeric) |>
-        # Calculate the amount consumed per AFE by dividing the amount consumed by the AFE factor
-        # dplyr::mutate(amountConsumedInGPerAfe = amountConsumedInG / afeFactor) |>
-        # Balance consumption over the recall period i.e. 7 days for this data
-        # dplyr::mutate(amountConsumedInGPerAfe = amountConsumedInG / 7) |> # Trying without this line
-        # Calculate the MNs content by multiplying the amount consumed per AFE by the nutrient composition for each nutrient
-        # TODO: This is the possible source of the errors. Check the calculation formula
-        # NOTE: Update 28/02/2024: The micronutrient composition is in 100g of the food item. We need to divide by 100 to get the nutrient content per gram see Andy's email
-        dplyr::mutate(dplyr::across(MNList, ~ . / afeFactor)) |> # NOTE This is a trial of Andy's suggestion
-        # Group the data by householdId so that we can summarise the data at the household level
+        dplyr::mutate(dplyr::across(MNList, ~ . / afeFactor)) |>
         dplyr::group_by(householdId) |>
-        # Summarise the data to get the total nutrient intake per afe per day for each household and name the columns with the nutrient name and "Supply"
-        # dplyr::summarize(dplyr::across(all_of(MNList), ~ sum(.x, na.rm = TRUE), .names = "{.col}Supply")) |> # TODO: Divide by the afe factor here
-        # NOTE: Trial of Andy's suggestion
-        dplyr::summarize(dplyr::across(dplyr::all_of(MNList), ~ sum(.x / 100 * amountConsumedInG, na.rm = TRUE), .names = "{.col}Supply")) |> # TODO: Divide by the afe factor here
-        # The summaries remove the household details so we need to join them back to the data
+        dplyr::summarize(dplyr::across(dplyr::all_of(MNList), ~ sum(.x / 100 * amountConsumedInG, na.rm = TRUE), .names = "{.col}Supply")) |>
         dplyr::left_join(householdDetailsDf) |>
-        # Bind thresholds to the data. The thresholds data has one row so it should be recycled to the number of rows in the data
         dplyr::bind_cols(upperIntakeLevel)
 
-    # Create adequacy columns for each nutrient
-    # NOTE: This code is not pretty and can be improved. It works for now
     for (nutrient in MNList) {
-        # Create the supply column for each nutrient
         supply_col <- paste0(nutrient, "Supply")
-        # Create the threshold column for each nutrient
-        threshold_col <- paste0(nutrient, "AfeUpperIntakeLevel") # Adjust if the naming convention is different
-
-        # Create the adequacy column for each nutrient to store the adequacy of the nutrient supply
+        threshold_col <- paste0(nutrient, "AfeUpperIntakeLevel")
         adequacy_col <- paste0(nutrient, "HighIntakeRisk")
 
-        # Only create the adequacy column if enrichedHouseholdConsumption[[threshold_col]] is not NA.
         if (!is.na(effectivenessCalculations::getMnThresholds(intakeThresholds, nutrient, "ul"))) {
             enrichedHouseholdConsumption[[adequacy_col]] <- ifelse(enrichedHouseholdConsumption[[supply_col]] >= effectivenessCalculations::getMnThresholds(intakeThresholdsDf, nutrient, "ul"), 1, 0)
         }
     }
 
-    # Prevalence of Inadequacy Summaries
-    # Households count summaries
     statsHouseholdCount <- enrichedHouseholdConsumption |>
-        # Re_group the data by district_name and adequacy to get the total number of households with adequate and inadequate Vitamin A intake
         dplyr::group_by(dplyr::across(dplyr::all_of(aggregationGroup))) |>
-        # Summarise the data to get the total number of households with adequate and inadequate Vitamin A intake
         dplyr::summarize(households = dplyr::n())
 
-    ## Count Adequate and Inadequate
     statsCountHighIntakeRisk <- enrichedHouseholdConsumption |>
         dplyr::group_by(dplyr::across(dplyr::all_of(aggregationGroup))) |>
         dplyr::summarize(dplyr::across(dplyr::ends_with("HighIntakeRisk"), ~ sum(.x, na.rm = TRUE), .names = "{.col}HighIntakeRiskCount"))
 
-    ## Count Inadequate
     statsCountLowIntakeRisk <- enrichedHouseholdConsumption |>
         dplyr::group_by(dplyr::across(dplyr::all_of(aggregationGroup))) |>
         dplyr::summarize(dplyr::across(dplyr::ends_with("HighIntakeRisk"), ~ sum(1 - .x, na.rm = TRUE), .names = "{.col}LowIntakeRiskCount"))
 
-    # Percentage Inadequate
     statsPercentageHighIntakeRisk <- statsHouseholdCount |>
         dplyr::left_join(statsCountHighIntakeRisk) |>
         dplyr::mutate(dplyr::across(dplyr::ends_with("HighIntakeRiskCount"), ~ round((.x * 100 / households), 2), .names = "{.col}PercHighIntakeRisk"))
 
-    ## Median Supply
     statsMedianSupply <- enrichedHouseholdConsumption |>
         dplyr::group_by(dplyr::across(dplyr::all_of(aggregationGroup))) |>
         dplyr::summarize(dplyr::across(dplyr::ends_with("Supply"), ~ round(median(.x, na.rm = TRUE), 0), .names = "{.col}MedianSupply"))
 
-    ## baselineAdequacyPrevalence
-    # Merge the stats data into one dataframe
     baselineAdequacyPrevalence <- statsHouseholdCount |>
         dplyr::left_join(statsCountHighIntakeRisk) |>
         dplyr::left_join(statsPercentageHighIntakeRisk) |>
         dplyr::left_join(statsMedianSupply) |>
         dplyr::bind_cols(upperIntakeLevel)
 
-    # Get the column order for the data
     columnOrder <- sort(names(baselineAdequacyPrevalence))
 
-    # Reorder the columns for better readability
     baselineAdequacyPrevalence <- baselineAdequacyPrevalence |>
         dplyr::select(dplyr::all_of(columnOrder)) |>
         dplyr::select(aggregationGroup, households, dplyr::everything())
 
     return(baselineAdequacyPrevalence)
 }
+
 
