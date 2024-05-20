53119fc2cc9663e6a9754aa859f094fba05884dd
dzvoti <libertydzvotimlambo@gmail.com>
2024-05-20 12:42:33 +0100

    refactor: moved the `previewData` function from `functions.R` to `previewData.R` file

--- R/functions.R
+++ R/functions.R
@@ -45,42 +45,6 @@ calculateConsPerAfe <- function(df, consumptionCol, afeCol) {
     return(df)
 }
 
-
-
-#' Preview the First 1000 Rows of a Dataframe
-#'
-#' This function previews the first 1000 rows of a dataframe using DT::datatable.
-#' The name of the dataframe is used as the caption, which is left-aligned.
-#'
-#' @param df A dataframe to preview.
-#'
-#' @return A DT::datatable object showing the first 1000 rows of the input dataframe.
-#'
-#' @examples
-#' # Create a dataframe
-#' df <- data.frame(
-#'     x = rnorm(5000),
-#'     y = rnorm(5000),
-#'     z = rnorm(5000)
-#' )
-#' # Preview the dataframe
-#' previewData(df)
-#'
-#' @export
-previewData <- function(df) {
-    df |>
-        head(1000) |>
-        # Use the name of the dataframe as the caption. Left align the caption
-        DT::datatable(
-            caption = htmltools::tags$caption(
-                style = "caption-side: left; text-align: left;",
-                paste0("Preview of the ", deparse(substitute(df)))
-            )
-        )
-}
-
-
-
 # #' Calculate Food Vehicle Household Reach
 # #'
 # #' This function calculates the household reach for each food vehicle. It filters the fortifiable food items to get the food vehicle, processes the consumption data, and then calculates the household reach.
---
+++ R/previewData.R
@@ -0,0 +1,31 @@
+#' Preview the First 1000 Rows of a Dataframe
+#'
+#' This function previews the first 1000 rows of a dataframe using DT::datatable.
+#' The name of the dataframe is used as the caption, which is left-aligned.
+#'
+#' @param df A dataframe to preview.
+#'
+#' @return A DT::datatable object showing the first 1000 rows of the input dataframe.
+#'
+#' @examples
+#' # Create a dataframe
+#' df <- data.frame(
+#'     x = rnorm(5000),
+#'     y = rnorm(5000),
+#'     z = rnorm(5000)
+#' )
+#' # Preview the dataframe
+#' previewData(df)
+#'
+#' @keywords internal
+previewData <- function(df) {
+    df |>
+        head(1000) |>
+        # Use the name of the dataframe as the caption. Left align the caption
+        DT::datatable(
+            caption = htmltools::tags$caption(
+                style = "caption-side: left; text-align: left;",
+                paste0("Preview of the ", deparse(substitute(df)))
+            )
+        )
+}
