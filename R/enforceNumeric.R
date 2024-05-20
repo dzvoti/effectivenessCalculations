946c73f4573d3575135afd4774187445da37e9ba
dzvoti <libertydzvotimlambo@gmail.com>
2024-05-20 10:53:38 +0100

    refactor: moved the `enforceNumeric` function from `functions.R` to `enforceNumeric.R` file - also added a `@keywords internal` tag to the `enforceNumeric` function

---
+++ R/enforceNumeric.R
@@ -0,0 +1,20 @@
+#' Enforce Numeric Data Type for Specified Columns
+#'
+#' This function converts specified columns of a dataframe to numeric data type.
+#'
+#' @param df A dataframe.
+#' @param cols A vector of column names that should be converted to numeric.
+#'
+#' @return A dataframe with the specified columns converted to numeric.
+#'
+#' @examples
+#' \dontrun{
+#' df <- data.frame(a = c("1", "2", "3"), b = c("4", "5", "6"))
+#' df <- enforceNumeric(df, c("a", "b"))
+#' }
+#'
+#' @keywords internal
+enforceNumeric <- function(df, cols) {
+    df <- df |> dplyr::mutate_at(cols, as.numeric)
+    return(df)
+}
