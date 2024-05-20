4b2b2316631bf0bda55012a93370d44f340ddb82
dzvoti <libertydzvotimlambo@gmail.com>
2024-05-20 10:28:42 +0100

    refactor: moved the `checkData` function from `functions.R` to ` chechData.R` file - also added a @`keywords internal` tag to the `checkData` function

--- R/functions.R
+++ R/functions.R
@@ -32,27 +32,7 @@ loadCsvFiles <- function(folder_path) {
 
 
 
-#' Check if Required Data is Loaded
-#'
-#' This function checks if the required data ("householdConsumption", "householdDetails", and "nctList") is loaded in the global environment.
-#'
-#' @return A logical value indicating whether the required data is loaded. Returns TRUE if all required data is loaded, and FALSE otherwise.
-#'
-#' @examples
-#' \dontrun{
-#' checkData()
-#' }
-#'
-#' @export
-checkData <- function() {
-    # Check if the data is loaded
-    if (exists("householdConsumption") && exists("householdDetails") && exists("nctList")) {
-        return(TRUE)
-    } else {
-        print("All expected data not loaded, please load data first.")
-        return(FALSE)
-    }
-}
+
 
 
 
